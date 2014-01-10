525e61bd1f2edd96eea6f9f3aca389a53cbc0938
Benjamin Larson <benjamin.larson@compellent.com>
2014-01-10 14:05:17 -0600

    Initial commit

---
+++ Cave/src/HelloDate.java
@@ -0,0 +1,10 @@
+
+import java.util.Date;
+
+public class HelloDate {
+    public static void main (String[] args) {
+        System.out.println ("The current date is: ");
+        System.out.println(new Date());
+    }
+
+}
