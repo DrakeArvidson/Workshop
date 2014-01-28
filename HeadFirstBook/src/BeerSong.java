b549f90cadfaba0272117291dd1124e990399d8b
Benjamin Larson <benjamincrist8@gmail.com>
2014-01-28 12:58:59 -0600

    adding some more from HeadFirst book

--- HeadFirstBook/src/BeerSong.java
+++ HeadFirstBook/src/BeerSong.java
@@ -12,9 +12,20 @@ public class BeerSong {
 			  word = "bottle"; //singular, as in ONE bottle
 		  }
 
-		  System.out.println(beerNum + " " + word + " of beer on the wall");
-	
-	  }
-	}
-}
+          System.out.println(beerNum + " " + word + " of beer on the wall");
+          System.out.println(beerNum + " " + word + " of beer.");
+          System.out.println("Take one down.");
+          System.out.println("Pass it around.");
+          beerNum = beerNum - 1;
+      } //end while loop
+
+          if (beerNum > 0 ) {
+              System.out.println(beerNum + " " + word + " of beer on the wall");
+          }
+          else {
+              System.out.println("No more bottles of beer on the wall");
+          } //end else
+
+	} //end main method
+}//class
 
