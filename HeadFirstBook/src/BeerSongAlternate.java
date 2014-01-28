0210a0c38a164fa458436cda70d4674a57388a91
Benjamin Larson <benjamincrist8@gmail.com>
2014-01-28 13:08:31 -0600

    adding new version of BeerSong - this version will be designed to output lyrics in format posted here: http://www.99-bottles-of-beer.net/lyrics.html

---
+++ HeadFirstBook/src/BeerSongAlternate.java
@@ -0,0 +1,41 @@
+/**
+ * Created by blarson on 1/28/14.
+ * This is a modification of BeerSong.java to match lyrics listed here:
+ * http://www.99-bottles-of-beer.net/lyrics.html
+ * This makes it easier to match an expected output
+ */
+public class BeerSongAlternate {
+    public static void main (String[] args) {
+        //we are creating a var named beerNum
+        int beerNum = 99;
+        //we created a String named "word"
+        String word = "bottles";
+
+        while (beerNum > 0) {
+
+            if (beerNum == 1) {
+                word = "bottle"; //singular, as in ONE bottle
+            }
+
+//            System.out.println(beerNum + " " + word + " of beer on the wall");
+//            System.out.println(beerNum + " " + word + " of beer.");
+//            System.out.println("Take one down.");
+//            System.out.println("Pass it around.");
+
+            System.out.println(beerNum + " "+ word + " of beer on the wall, " + beerNum + " " + word + " of beer.");
+            System.out.println("Take one down and pass it around, " + beer)
+
+
+            beerNum = beerNum - 1;
+        } //end while loop
+
+        if (beerNum > 0 ) {
+            System.out.println(beerNum + " " + word + " of beer on the wall");
+        }
+        else {
+            System.out.println("No more bottles of beer on the wall");
+        } //end else
+
+    } //end main method
+}//class
+
