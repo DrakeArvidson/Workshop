b9fbb32d4c52f3807405ff7c374495c64559df05
Benjamin Larson <benjamincrist8@gmail.com>
2014-02-17 10:42:44 -0600

    Added some things from NewBoston

---
+++ Cave/src/apples.java
@@ -0,0 +1,10 @@
+/**
+ * Created by blarson on 2/14/14.
+ */
+public class apples {
+    public static void main (String[] args) {
+
+        tuna tunaObject = new tuna();
+        tunaObject.simpleMessage();
+    }
+}
---
+++ Cave/src/tuna.java
@@ -0,0 +1,8 @@
+/**
+ * Created by blarson on 2/14/14.
+ */
+public class tuna {
+    public void simpleMessage() {
+        System.out.println("this is another class");
+    }
+}
