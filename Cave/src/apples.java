e7022999b81273f87a4ab484ac1d80affee23e7e
Benjamin Larson <benjamincrist8@gmail.com>
2014-03-10 09:55:41 -0500

    Modified code  from New Boston

--- Cave/src/apples.java
+++ Cave/src/apples.java
@@ -7,13 +7,13 @@ import java.util.Random;
 
 class apples {
     public static void main (String[] args) {
-        System.out.println("Index\tValue");
-        int bucky[]={32,12,18,54,2};
+        int bucky[]={21, 16, 86,21,3};
+        int sum = 0;
 
         for (int counter = 0; counter < bucky.length; counter++) {
-            System.out.println(counter + "\t\t" + bucky[counter]);
-
+            sum += bucky[counter];
         }
+        System.out.println("The sum of these number is: " + sum);
 
 
 
