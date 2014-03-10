8977888c8bf09598b7a5424533495ab8cc2339f9
Benjamin Larson <benjamincrist8@gmail.com>
2014-03-10 13:01:21 -0500

    Modified code  from New Boston

--- Cave/src/apples.java
+++ Cave/src/apples.java
@@ -2,28 +2,28 @@
  * Created by blarson on 2/14/14.
  */
 
-
-
 class apples {
+
+
     public static void main (String[] args) {
-        int bucky[]={3,4,5,6,7};
-        int total = 0;
-        int altTotal = 0;
+        int bucky [] = {3,4,5,6,7};
 
-        for(int x: bucky) {
-            total += x;
+        for (int y:bucky) {
+            System.out.println(y);
         }
 
-        System.out.println("total is: " + total);
+        change(bucky);
 
-
-        for (int counter = 0; counter < bucky.length; counter ++ ) {
-            altTotal = altTotal + bucky[counter];
-            //altTotal += bucky[counter];
+        for (int y:bucky) {
+            System.out.println(y);
         }
-
-        System.out.println("altTotal is: " + altTotal);
-
-
     }
+
+    public static void change(int x[]) {
+        for (int counter = 0; counter < x.length ; counter++) {
+            x[counter] += 10;
+        }
+    }
+
+
 }
