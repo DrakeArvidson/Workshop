9116935cd10b4371105b68c1a036407904d7f354
Benjamin Larson <benjamincrist8@gmail.com>
2014-03-10 10:05:44 -0500

    Modified code  from New Boston

--- Cave/src/apples.java
+++ Cave/src/apples.java
@@ -2,22 +2,25 @@
  * Created by blarson on 2/14/14.
  */
 
-import java.util.Scanner;
 import java.util.Random;
 
 class apples {
     public static void main (String[] args) {
-        int bucky[]={21, 16, 86,21,3};
-        int sum = 0;
+        //creating random generator object and array object
+        Random rand = new Random();
+        int frequency[]=new int [7];
 
-        for (int counter = 0; counter < bucky.length; counter++) {
-            sum += bucky[counter];
+        //roll dice 1000 times, increase each index 1-6 when that value is generated
+        for (int roll = 1; roll < 1000; roll++) {
+            ++frequency[1+rand.nextInt(6)];
         }
-        System.out.println("The sum of these number is: " + sum);
 
+        //print header for table
+        System.out.println("Face\tFrequency");
 
-
-
-
+        //print values for entire array, excluding first element since 0 can't be rolled on a die
+        for (int face = 1; face < frequency.length ; face++){
+            System.out.println(face + "\t\t" + frequency[face]);
+        }
     }
 }
