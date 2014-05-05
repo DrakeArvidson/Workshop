f6d3bf4c255999575ee81289994ee5b615456668
Benjamin Larson <benjamincrist8@gmail.com>
2014-05-05 07:39:43 -0500

    trivial change to adder

--- Cave/src/Adder.java
+++ Cave/src/Adder.java
@@ -1,11 +1,3 @@
-/******************************************************************************
- *                       COPYRIGHT 2002 - 2012 BY DELL INC.
- *                          ALL RIGHTS RESERVED
- *
- * THIS DOCUMENT OR ANY PART OF THIS DOCUMENT MAY NOT BE REPRODUCED WITHOUT
- * WRITTEN PERMISSION FROM DELL INC.
- *****************************************************************************/
-
 import java.util.Scanner;
 
 /**
