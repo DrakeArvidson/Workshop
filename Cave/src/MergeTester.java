4ea52a59afc9ac039a125e1f6d1446db010f85e6
Benjamin Larson <benjamincrist8@gmail.com>
2014-05-19 13:26:40 -0500

    first commit of MergeTester

---
+++ Cave/src/MergeTester.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+ *                       COPYRIGHT 2002 - 2012 BY DELL INC.
+ *                          ALL RIGHTS RESERVED
+ *
+ * THIS DOCUMENT OR ANY PART OF THIS DOCUMENT MAY NOT BE REPRODUCED WITHOUT
+ * WRITTEN PERMISSION FROM DELL INC.
+ *****************************************************************************/
+
+/**
+ * @author blarson
+ */
+public class MergeTester {
+
+    /**
+     * @args command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is a test of initial version");
+    }
+
+}
