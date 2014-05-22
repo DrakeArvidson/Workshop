429a00d58592dbbb51c561bcd798db0f87ff5948
Benjamin Larson <benjamincrist8@gmail.com>
2014-05-22 14:14:45 -0500

    added GreatestCommonDivisor.java and will make the program have more than one method in GreatestCommonDivisorMethod.java

---
+++ CollegeBook/src/Methods/GreatestCommonDivisor.java
@@ -0,0 +1,39 @@
+/******************************************************************************
+ *                       COPYRIGHT 2002 - 2012 BY DELL INC.
+ *                          ALL RIGHTS RESERVED
+ *
+ * THIS DOCUMENT OR ANY PART OF THIS DOCUMENT MAY NOT BE REPRODUCED WITHOUT
+ * WRITTEN PERMISSION FROM DELL INC.
+ *****************************************************************************/
+
+package Methods;
+
+import java.util.Scanner;
+
+/**
+ * @author blarson
+ */
+public class GreatestCommonDivisor {
+
+    /**
+     * @args command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter first integer: ");
+        int n1 = input.nextInt();
+        System.out.println("Enter second integer: ");
+        int n2 = input.nextInt();
+
+        int gcd = 1;
+        int k = 2;
+        while (k <= n1 && k <= n2) {
+            if (n1 % k == 0 & n2 % k ==0)
+                gcd = k;
+            k++;
+        }
+
+        System.out.println("The greatest common divisor for " + n1 + " and " + n2 + " is " + gcd);
+    }
+}
