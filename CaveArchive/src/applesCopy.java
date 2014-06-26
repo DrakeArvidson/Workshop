eb1445b7bb70c937654d224706d6e865df2551df
Benjamin Larson <benjamincrist8@gmail.com>
2014-06-26 12:45:39 -0500

    removed copywrite notice that shouldn't have been there in the first place

--- CaveArchive/src/applesCopy.java
+++ CaveArchive/src/applesCopy.java
@@ -1,14 +1,4 @@
-/******************************************************************************
- *                       COPYRIGHT 2002 - 2012 BY DELL INC.
- *                          ALL RIGHTS RESERVED
- *
- * THIS DOCUMENT OR ANY PART OF THIS DOCUMENT MAY NOT BE REPRODUCED WITHOUT
- * WRITTEN PERMISSION FROM DELL INC.
- *****************************************************************************/
 
-/**
- * @author blarson
- */
 
 import java.util.Scanner;
 
